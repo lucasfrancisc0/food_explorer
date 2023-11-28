9995def25402a279c99c6f2d134790f6b3b32d1f
Lucas <lucasarnoldd@gmail.com>
2023-11-28 14:29:52 -0300

    Created GlobalStyles

---
+++ web/src/styles/global.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+    font-size: 62.5%;
+  }
+
+  body, button, input {
+    font-family: ${({ theme }) => theme.FONTS.MAIN_TEXT};
+    font-size: ${({ theme }) => theme.FONT_SIZE.REGULAR};
+    font-weight: 400;
+
+    color: ${({ theme }) => theme.COLORS.LIGH_100};
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.COLORS.DARK_400};
+  }
+
+  button:hover {
+    transition: .5s;
+    filter: brightness(0.9);
+  }
+
+`;
--- web/src/styles/theme.js
+++ web/src/styles/theme.js
@@ -1,37 +1,54 @@
 export default {
   COLORS: {
-    "LIGHT_100": "#FFFFFF",
-    "LIGHT_200": "#FFFAF1",
-    "LIGHT_300": "#E1E1E6",
-    "LIGHT_400": "#C4C4CC",
-    "LIGHT_500": "#7C7C8A",
-    "LIGHT_600": "#76797B",
-    "LIGHT_700": "#4D585E",
+    LIGHT_100: "#FFFFFF",
+    LIGHT_200: "#FFFAF1",
+    LIGHT_300: "#E1E1E6",
+    LIGHT_400: "#C4C4CC",
+    LIGHT_500: "#7C7C8A",
+    LIGHT_600: "#76797B",
+    LIGHT_700: "#4D585E",
 
-    "DARK_100": "#000405",
-    "DARK_200": "#00070A",
-    "DARK_300": "#000204",
-    "DARK_400": "#000A0F",
-    "DARK_500": "#000C12",
-    "DARK_600": "#00111A",
-    "DARK_700": "#001119",
-    "DARK_800": "#0D161B",
-    "DARK_900": "#0D1D25",
-    "DARK_1000": "#192227",
+    DARK_100: "#000405",
+    DARK_200: "#00070A",
+    DARK_300: "#000204",
+    DARK_400: "#000A0F",
+    DARK_500: "#000C12",
+    DARK_600: "#00111A",
+    DARK_700: "#001119",
+    DARK_800: "#0D161B",
+    DARK_900: "#0D1D25",
+    DARK_1000: "#192227",
 
-    "TOMATO_100": "#750310",
-    "TOMATO_200": "#92000E",
-    "TOMATO_300": "#AB222E",
-    "TOMATO_400": "#AB4D55",
-    "TOMATO_500": "#FEE2E2",
+    TOMATO_100: "#750310",
+    TOMATO_200: "#92000E",
+    TOMATO_300: "#AB222E",
+    TOMATO_400: "#AB4D55",
+    TOMATO_500: "#FEE2E2",
 
-    "CARROT_100": "#FBA94C",
+    CARROT_100: "#FBA94C",
 
-    "MINT_100": "#04D361",
+    MINT_100: "#04D361",
 
-    "CAKE_100": "065E7C",
-    "CAKE_200": "#82F3FF",
+    CAKE_100: "065E7C",
+    CAKE_200: "#82F3FF",
 
-    "PURPLE_100": "#FF82FF"
-  }
+    PURPLE_100: "#FF82FF",
+
+  },
+
+    FONT_FAMILY: {
+      MAIN_TEXT: "font-family: 'Poppins', sans-serif",
+      TITLE_TEXT: "'Roboto', sans-serif",
+    },
+
+    FONT_SIZE: {
+      XSMALL: "1rem",
+      SMALL: "1.4rem",
+      REGULAR: "1.6rem",
+      MEDIUM: "2rem",
+      LARGE: "2.4rem",
+      XLARGE: "3.2rem",
+      XXLARGE: "4rem",
+    },
+    
 }
